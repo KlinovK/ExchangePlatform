317d2850860aad3b28a2d10309f5930d49c02e4d
kost9klinov <kost9klinov>
2018-08-27 22:00:45 +0300

    added DataService

---
+++ ExchangePlatform/Services/DataService.swift
@@ -0,0 +1,52 @@
+//
+//  DataService.swift
+//  ExchangePlatform
+//
+//  Created by Константин Клинов on 8/27/18.
+//  Copyright © 2018 Константин Клинов. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_COMPANY_USERS = DB_BASE.child("company_users")
+    private var _REF_ORDERS = DB_BASE.child("orders")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_COMPANY_USERS: DatabaseReference {
+        return _REF_COMPANY_USERS
+    }
+    
+    var REF_ORDERS: DatabaseReference {
+        return _REF_ORDERS
+    }
+    
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
+    
+    func createDBCompanyUser(uid: String, userData: Dictionary<String, Any>){
+        REF_COMPANY_USERS.child(uid).updateChildValues(userData)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
