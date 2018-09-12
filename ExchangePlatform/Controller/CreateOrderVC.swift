bfaf8a349754b9cf4e4c75f4d5ce063f089f9a26
kost9klinov <kost9klinov>
2018-09-12 21:09:01 +0300

    added search users by email

--- ExchangePlatform/Controller/CreateOrderVC.swift
+++ ExchangePlatform/Controller/CreateOrderVC.swift
@@ -18,19 +18,34 @@ class CreateOrderVC: UIViewController {
     @IBOutlet weak var doneBtn: UIButton!
     @IBOutlet weak var orderMemeberLbl: UILabel!
     
+    var emailArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
-        
+        emailSearchTxtField.delegate = self
+        emailSearchTxtField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
     }
 
+    @objc func textFieldDidChange(){
+        if emailSearchTxtField.text == "" {
+            emailArray = []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmail(forSearchQuery: emailSearchTxtField.text!) { (returnedEmailArray) in
+                self.emailArray = returnedEmailArray
+                self.tableView.reloadData()
+            }
+        }
+    }
 
     @IBAction func doneBtnWasPressed(_ sender: Any) {
     }
     
     @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
 
@@ -43,14 +58,17 @@ extension CreateOrderVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return emailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else {return UITableViewCell()}
        let profileImage = UIImage(named: "user")
-        cell.configureCell(profileImage: profileImage!, email: "random@gmail.com", isSelected: true)
+        cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: true)
         return cell
     }
+    
+}
+extension CreateOrderVC: UITextFieldDelegate {
     
 }
