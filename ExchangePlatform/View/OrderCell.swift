f7677f7aab007f414999846dd4e45e49de634099
kost9klinov <kost9klinov>
2018-09-23 19:16:35 +0300

    added addressTo and addressFrom to order cell

--- ExchangePlatform/View/OrderCell.swift
+++ ExchangePlatform/View/OrderCell.swift
@@ -13,11 +13,15 @@ class OrderCell: UITableViewCell {
     @IBOutlet weak var orderNumberLbl: UILabel!
     @IBOutlet weak var orderDescriptionLbl: UILabel!
     @IBOutlet weak var memberCountLbl: UILabel!
+    @IBOutlet weak var addressTo: UILabel!
+    @IBOutlet weak var addressFrom: UILabel!
     
-    func configureCell(number: String, description: String, memberCount: Int) {
+    func configureCell(number: String, description: String, memberCount: Int, addressFrom: String, addressTo: String) {
         self.orderNumberLbl.text = number
         self.orderDescriptionLbl.text = description
         self.memberCountLbl.text = "\(memberCount)"
+        self.addressFrom.text = addressFrom
+        self.addressTo.text = addressTo
     }
     
 }
