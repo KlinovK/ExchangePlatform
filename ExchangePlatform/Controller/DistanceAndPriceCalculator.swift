9dc54061f5347d5c68167c63b56ef4f42ae077b3
kost9klinov <kost9klinov>
2018-11-01 16:39:35 +0300

    fixed minor bugs

--- ExchangePlatform/Controller/DistanceAndPriceCalculator.swift
+++ ExchangePlatform/Controller/DistanceAndPriceCalculator.swift
@@ -16,16 +16,31 @@ class DistanceAndPriceCalculator: UIViewController {
     @IBOutlet weak var descLbl: UILabel!
     @IBOutlet weak var priceTxTField: InsetTextField!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+      
+        
     }
     
 
     @IBAction func segmentControlWasPressed(_ sender: Any) {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            descLbl.text = "Per km";
+        case 1:
+            descLbl.text = "Per transportation";
+        default:
+            break
+        }
     }
     
     @IBAction func calculateBtnWasPressed(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+  //          resultLbl.text = ((priceTxTField.text)! as NSString).doubleValue * 2
+        }
     }
     
 }
